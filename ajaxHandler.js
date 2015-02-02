399c68cab7e16d8992941bf8ce39c473b7b1eacd
marcel roa <marcel@debian-marcel.roa.de>
2015-02-02 17:34:16 +0100

    first

--- ajaxHandler.js
+++ ajaxHandler.js
@@ -1,1 +1,13 @@
- 
+function ajaxCall (url) {
+    var jqxhr = $.ajax(url)
+                 .done(function() {
+                     alert( "success" );
+                 })
+                 .fail(function() {
+                     alert( "error" );
+                 })
+                 .always(function() {
+                     alert( "complete" );
+                 });
+    return jqxhr;
+}
